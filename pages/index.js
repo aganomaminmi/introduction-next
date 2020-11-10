6ac7f377a690fe91ff6ecf5a72fe7c6da61de573
Kohta Takanami <ripplekouha@gmail.com>
2020-11-10 18:41:16 +0900

    [add]nextプロジェクトを作成

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+
+export default () => <div>
+  <style jsx>{`
+  h1 {
+    font-size: 72pt;
+    font-weight: bold;
+    text-align: right;
+    letter-spacing: -8px;
+    color: #f0f0f0;
+    margin: -40px 0px;
+  }
+
+
+  p {
+    margin: 0px;
+    color: #066;
+    font-size: 16pt;
+  }
+
+  `}</style>
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!</p>
+  <p>これはビルトインCSSによるスタイルです。</p>
+</div>
