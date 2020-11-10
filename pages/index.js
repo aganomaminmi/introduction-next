a4fa7c78ea998ff5e65c0d24365de15ea92a8a5e
Kohta Takanami <ripplekouha@gmail.com>
2020-11-10 20:09:21 +0900

    [add]nextでコンポーネントを使ってみる

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,10 @@
-import Link from 'next/link';
+import Counter from '../components/Counter';
 import style from '../static/Style';
-
-const h1 = {
-  fontSize: '72pt',
-  fontWeight: 'bold',
-  textAlign: 'right',
-  letterSpacing: '-8px',
-  color: '#f0f0f0',
-  margin: '-40px 0px'
-}
-
-
-const p = {
-  margin: '0px',
-  color: '#066',
-  fontSize: '16pt'
-}
 
 export default () => <div>
   {style}
   <h1>Next.js</h1>
   <p>Welcome to next.js!</p>
   <hr />
-  <div>
-    <Link href="/other">
-      <button>Go to Other page &gt;&gt;</button>
-    </Link>
-  </div>
+  <Counter />
 </div>
