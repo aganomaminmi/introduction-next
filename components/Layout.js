f1f86b3e0212083823dc735277b489b9286d3344
Kohta Takanami <ripplekouha@gmail.com>
2020-11-11 21:25:16 +0900

    [fix]layoutコンポーネントを作成

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import React, { Compnent } from 'react';
+import Header from './Header';
+import Footer from './Footer';
 import Style from '../static/Style';
 
 class Layout extends Component {
 
   render() {
-    {style}
+    return (
+      <div>
+        {style}
+        <Header header={this.props.header} title={this.props.title} />
+        {this.props.childred}
+        <Footer footer="copyright SYODA-Tuyano." />
+      </div>
+    );
   }
 }
+
+export default Layout;
