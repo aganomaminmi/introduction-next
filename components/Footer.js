f40e9e0d7c4a5e3ae40fab221a9e9388b407a063
Kohta Takanami <ripplekouha@gmail.com>
2020-11-11 21:21:09 +0900

    [add]フッターコンポーネントを作成

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render9) {
+    return (
+      <footer>
+        <div>{this.props.footer}</div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
