3eeb3ff25b038a6214fe8b32e5cee671f0acc053
Kohta Takanami <ripplekouha@gmail.com>
2020-11-14 21:41:29 +0900

    counterコンポーネントをページに配置

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import Image from "../static/imgComponents/Image";
+import Counter from "../components/Counter";
 
 export default () => (
   <Layout header="Next" title="Top page.">
     <p>Welcome to next.js!</p>
       <Image fname="logo_kintone_mark_rgb.png" size="250" />
+      <Counter />
     <hr/>
     <Link href="./other">
       <button> go to Other &gt;&gt;</button>
--- pages/other.js
+++ pages/other.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import Layout from '../components/Layout';
+import Counter from "../components/Counter";
 
 export default () => (
   <Layout header="other" title= "Other page.">
     <p>This is Other page.</p>
+    <Counter />
     <hr />
     <div>
       <Link href="/">
