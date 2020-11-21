f9f2f4b0781852bb32de4ba3cf5f2efbd5ceae2e
Kohta Takanami <ripplekouha@gmail.com>
2020-11-22 00:57:05 +0900

    [add]firebaseを初期化

---
+++ pages/fire.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import Layout from "../components/Layout";
+import SampleData from '../components/Sampledata';
+
+export default () => (
+    <Layout header="fire" title="Sample data.">
+        <SampleData />
+    </Layout>
+);
--- store.js
+++ store.js
@@ -1,5 +1,27 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVnxxVmdzeq5ghb3n5BQchPHT3jz6vYJ8",
+    authDomain: "takanami-react-app.firebaseapp.com",
+    databaseURL: "https://takanami-react-app.firebaseio.com",
+    projectId: "takanami-react-app",
+    storageBucket: "takanami-react-app.appspot.com",
+    messagingSenderId: "911520889118",
+    appId: "1:911520889118:web:a52b2d9b033669de28310f",
+    measurementId: "G-T2VFB0PTTY"
+};
+
+let fireapp;
+try {
+    firebase.initializeApp(firebaseConfig);
+} catch (error) {
+    console.log(error.message);
+}
+
+// export default fireapp;
+
 
 const initial = {
     message: 'START',
