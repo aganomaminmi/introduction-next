513a7ac599fb36c83580c60e40878fc54b8a468f
Kohta Takanami <ripplekouha@gmail.com>
2020-11-22 01:32:47 +0900

    [add]firebase検索機能を作成

---
+++ pages/fire_find.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+import FireFind from '../components/Firefind';
+
+export default () => (
+    <Layout header="fire" title="Sample data.">
+        <FireFind />
+    </Layout>
+)
